0aadf37973a81ef22dd5ab24f44747a8c7d31048
System Administrator <root@CN0105.local>
2019-09-11 15:07:18 +0100

    Updates

---
+++ precache-manifest.120572c854cbb5b8e5d04c61f918a017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcdda68c0f60c7aafed3b2881f63e0e",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "7fa6c170fa563c5221b5",
+    "url": "/react-calc/static/css/main.cb63988b.chunk.css"
+  },
+  {
+    "revision": "2f5f5530455f9bf3aafb",
+    "url": "/react-calc/static/js/2.eca8df60.chunk.js"
+  },
+  {
+    "revision": "7fa6c170fa563c5221b5",
+    "url": "/react-calc/static/js/main.42c930f9.chunk.js"
+  },
+  {
+    "revision": "1668573c18ae934e7a14",
+    "url": "/react-calc/static/js/runtime~main.f80c8a1b.js"
+  }
+]);
